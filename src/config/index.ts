06532455d5f84faa24cf956a8bc494ec95908dab
Tomcaine <Tomcaine@der-kaplinski.de>
2023-12-18 00:30:07 +0100

    Level variablen der Config hinzugefügt

--- src/config/index.ts
+++ src/config/index.ts
@@ -39,5 +39,12 @@ export default {
     log: data.channels.log,
     roleExplain: data.channels.roleExplain,
     notification: data.channels.notification,
+  },
+  level: {
+    xpPerLevel: data.level.xp_per_level,
+    multiplikator: data.level.multiplikator,
+    exponent: data.level.exponent,
+    cooldownTime: data.level.cooldownTime,
   }
 }
+
